46bb7d16c0a80629ca74cda5c31bde970cae0eeb
Victor <vatorresm88@gmail.com>
2021-01-02 14:48:26 -0500

    Connecting and migrating the database

--- PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
@@ -1,16 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PaymentAPI.Models
 {
-    public class PaymentDetailContext: DbContext
+    public class PaymentDetailContext : DbContext
     {
-        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
         {
 
         }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
     }
 }
