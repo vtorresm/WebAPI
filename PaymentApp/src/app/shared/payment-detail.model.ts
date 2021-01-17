6f5961da9bac51cfecf576f3ac47bf4bedf4c4f1
Victor <vatorresm88@gmail.com>
2021-01-17 14:43:01 -0500

    Creating the model

---
+++ PaymentApp/src/app/shared/payment-detail.model.ts
@@ -0,0 +1,9 @@
+export class PaymentDetail {
+
+    paymentDetailId: number;
+    cardOwnerName: string;
+    cardNumber: string;
+    expirationDate: string;
+    securityCode: string;
+
+}
--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PaymentDetail } from './payment-detail.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // readonly baseURL = 'http://localhost:61236/api/PaymentDetail'
+  formData: PaymentDetail = new PaymentDetail();
 }
