4d35695e045b2c0a172ee00f33439acae4717b60
Victor <vatorresm88@gmail.com>
2021-02-01 15:07:42 -0500

    Form Design in Angular

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
@@ -12,7 +15,9 @@ import { PaymentDetailFormComponent } from './payment-details/payment-detail-for
     PaymentDetailFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
