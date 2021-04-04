9e70c04ef56ad3a89c0b5c53fef403f5827bcc0a
Victor <vatorresm88@gmail.com>
2021-04-03 21:28:28 -0500

    Retrieve & Display records from Web API with Angular

--- PaymentApp/src/app/payment-details/payment-details.component.ts
+++ PaymentApp/src/app/payment-details/payment-details.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from '../shared/payment-detail.service';
 
 @Component({
   selector: 'app-payment-details',
   templateUrl: './payment-details.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PaymentDetailsComponent implements OnInit {
-
-  constructor() { }
+  constructor(public service: PaymentDetailService) {}
 
   ngOnInit(): void {
+    this.service.refreshList();
   }
-
 }
--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -9,10 +9,18 @@ import { PaymentDetail } from './payment-detail.model';
 export class PaymentDetailService {
   constructor(private http: HttpClient) {}
 
-  formData: PaymentDetail = new PaymentDetail();
   readonly baseURL = 'http://localhost:49898/api/PaymentDetail'; // 49898 61236
+  formData: PaymentDetail = new PaymentDetail();
+  list: PaymentDetail[];
 
   postPaymentDetail() {
     return this.http.post(this.baseURL, this.formData);
+  }
+
+  refreshList() {
+    this.http
+      .get(this.baseURL)
+      .toPromise()
+      .then((res) => (this.list = res as PaymentDetail[]));
   }
 }
